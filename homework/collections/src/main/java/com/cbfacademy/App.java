768bddecec47d5ff3041b2cb87659f4003ff25a6
AdelabuAderonke <adelabu.aderonke9638@gmail.com>
2023-08-07 17:30:48 +0100

    Collections homework done,passed all test

--- homework/collections/src/main/java/com/cbfacademy/App.java
+++ homework/collections/src/main/java/com/cbfacademy/App.java
@@ -1,7 +1,21 @@
 package com.cbfacademy;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // System.out.println("Hello World!");
+
+        CollectionsAssignment coll = new CollectionsAssignment();
+        ArrayList<Integer> num = new ArrayList<>(Arrays.asList(3, 5, 3, 6, 2, 4, 9, 12, 35, 78));
+        ArrayList<Integer> num2 = new ArrayList<>(Arrays.asList(12, 1, 4, 35, 78));
+        ArrayList<String> num3 = new ArrayList<>(Arrays.asList("Alice", "Bob", "Alice", "Alice", "Jon"));
+        System.out.println(coll.mostFrequent(num3));
+        // List<Integer> numbers = new ArrayList<>(Arrays.asList(3, 6, 9, 12, 35, 78))
+        //System.out.println(coll.removeSmallInts(num, 5));
+        // coll.containsDuplicates(num);
+        // System.out.println(coll.inEither(num,num2));
+        // System.out.println(coll.inBoth(num,num2));
     }
 }
