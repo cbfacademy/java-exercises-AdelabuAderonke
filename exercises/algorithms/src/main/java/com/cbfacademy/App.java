e688f4ed200e8de1e64ffc5a63fda4cebbb9397e
AdelabuAderonke <adelabu.aderonke9638@gmail.com>
2023-09-04 17:10:35 +0100

    Worked on Merge sort algortithm

--- exercises/algorithms/src/main/java/com/cbfacademy/App.java
+++ exercises/algorithms/src/main/java/com/cbfacademy/App.java
@@ -1,7 +1,35 @@
 package com.cbfacademy;
 
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+  public static void main(String[] args) {
+
+  }
+
+  // Using Merge sort
+  public void mergeSort(int[] arr, int left, int right) {
+
+    int mid = (left + right) / 2;
+    mergeSort(arr, left, mid);
+    mergeSort(arr, mid + 1, right);
+    merge(arr, left, mid, right);
+  }
+
+  public void merge(int[] arr, int left, int right, int mid) {
+    //sizes of the sub arrays
+    int first = mid - left + 1;
+    int second = right - mid;
+
+    // Create temporary arrays to hold the data
+    int[] leftArray = new int[first];
+    int[] rightArray = new int[second];
+
+    // Copy data to temporary arrays leftArray[] and rightArray[]
+    for (int i = 0; i < first; i++) {
+      leftArray[i] = arr[left + i];
     }
+    for (int j = 0; j < second; j++) {
+      rightArray[j] = arr[mid + 1 + j];
+    }
+
+  }
 }
