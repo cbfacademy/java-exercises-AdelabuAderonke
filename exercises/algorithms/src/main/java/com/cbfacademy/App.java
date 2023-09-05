b9cf21763dcee1cb87c86ae17037b223b72605dc
AdelabuAderonke <adelabu.aderonke9638@gmail.com>
2023-09-05 19:34:27 +0100

    linear search

--- exercises/algorithms/src/main/java/com/cbfacademy/App.java
+++ exercises/algorithms/src/main/java/com/cbfacademy/App.java
@@ -1,19 +1,18 @@
 package com.cbfacademy;
 
 public class App {
-  public static void main(String[]args){
-    String[] arr ={"in", "to","me","go"};
+  public static void main(String[] args) {
+    String[] arr = { "in", "to", "me", "go" };
     System.out.println(linearSearch(arr, "hu"));
   }
 
-  //using Linear Search
-  public static String linearSearch(String[] arr, String searchString){
-    for(int i=0; i < arr.length;i++){
-      if(arr[i] == searchString){
-        return "String found in index"+ i;
+  // using Linear Search
+  public static String linearSearch(String[] arr, String searchString) {
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] == searchString) {
+        return "String found in index" + i;
       }
     }
     return "String not found";
   }
 }
-  
