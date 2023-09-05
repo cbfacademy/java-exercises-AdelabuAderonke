4de809a32689e506c02aee5899624a72f5ff8490
AdelabuAderonke <adelabu.aderonke9638@gmail.com>
2023-09-05 15:48:46 +0100

    quick sort algorithm exercise

--- exercises/algorithms/src/main/java/com/cbfacademy/App.java
+++ exercises/algorithms/src/main/java/com/cbfacademy/App.java
@@ -2,74 +2,52 @@ package com.cbfacademy;
 
 public class App {
   public static void main(String[] args) {
-
     int[] arr = { 12, 11, 13, 5, 6, 7 };
-    System.out.print("Original Array:");
+
+    System.out.println("Original array:");
     for (int value : arr) {
-      System.out.print(value + " ");
+      System.out.print(value+" ");
     }
     System.out.println();
-    mergeSort(arr, 0, arr.length - 1);
-    
-    System.out.print("Sorted array: ");
-    for (int value : arr) {
-      System.out.print(value + " ");
+    quickSort(arr, 0, arr.length - 1);
+
+    System.out.println("\nSorted array:");
+   for (int value : arr) {
+      System.out.print(value +" ");
+      
+
     }
-    System.out.println(); // Add a newline for formatting
+    System.out.println();
+
   }
 
-  // Using Merge sort
-  public static void mergeSort(int[] arr, int left, int right) {
-    if (left < right) {
-      int mid = (left + right) / 2;
-      mergeSort(arr, left, mid);
-      mergeSort(arr, mid + 1, right);
-      merge(arr, left, mid, right);
+  public static void quickSort(int[] arr, int beg, int end) {
+    if (beg < end) {
+      int pivotIndex = partition(arr, beg, end);
+      quickSort(arr, beg, pivotIndex - 1);
+      quickSort(arr, pivotIndex + 1, end);
     }
   }
 
-  public static void merge(int[] arr, int left, int mid, int right) {
-    // sizes of the subarrays
-    int first = mid - left + 1;
-    int second = right - mid;
+  public static int partition(int[] arr, int beg, int end) {
+    int pivot = arr[end]; // Choose the pivot as the last element
+    int pIndex = beg - 1;
 
-    // Create temporary arrays to hold the data
-    int[] leftArray = new int[first];
-    int[] rightArray = new int[second];
-
-    // Copy data to temporary arrays leftArray[] and rightArray[]
-    for (int i = 0; i < first; i++) {
-      leftArray[i] = arr[left + i];
-    }
-    for (int j = 0; j < second; j++) {
-      rightArray[j] = arr[mid + 1 + j];
-    }
-
-    // Merge the two subarrays back into the original array
-    int i = 0, j = 0, k = left;
-    while (i < first && j < second) {
-      if (leftArray[i] <= rightArray[j]) {
-        arr[k] = leftArray[i];
-        i++;
-      } else {
-        arr[k] = rightArray[j];
-        j++;
+    for (int i = beg; i < end; i++) {
+      if (arr[i] < pivot) {
+        // Swap arr[i] and arr[pIndex+1]
+        int temp = arr[i];
+        arr[i] = arr[pIndex + 1];
+        arr[pIndex + 1] = temp;
+        pIndex++;
       }
-      k++;
     }
 
-    // Copy remaining elements of leftArray[] if any
-    while (i < first) {
-      arr[k] = leftArray[i];
-      i++;
-      k++;
-    }
+    int temp = arr[end];
+    arr[end] = arr[pIndex + 1];
+    arr[pIndex + 1] = temp;
 
-    // Copy remaining elements of rightArray[] if any
-    while (j < second) {
-      arr[k] = rightArray[j];
-      j++;
-      k++;
-    }
+    return pIndex + 1;
   }
+
 }
